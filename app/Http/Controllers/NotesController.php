e0c99daba6a00694b88cff63064a3c8ef8a6886a
Salmaan Saeed <salsaeed999@gmail.com>
2023-11-06 15:58:29 -0500

    specify the type of crud operation is each api

--- app/Http/Controllers/NotesController.php
+++ app/Http/Controllers/NotesController.php
@@ -10,11 +10,13 @@ class NotesController extends Controller
 {
     //
     public function get(string $id): Notes {
+        // Read
         $notes = Notes::findOrFail($id);
         return $notes;
     }
 
     public function create(Request $request): string {
+        // Create
         $note = new Notes;
         $note->title = $request->title;
         $note->description = $request->description;
